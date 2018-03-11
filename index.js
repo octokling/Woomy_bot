2ac92bc393bb2a573c2dfca5105a493347c16409
lolodu17 <34281237+lolodu17@users.noreply.github.com>
2018-03-11 11:55:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -455,7 +455,6 @@ bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
-  let prefix = !;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
