ece175009d93aa3422056e12708e72df160bb4aa
lolodu17 <34281237+lolodu17@users.noreply.github.com>
2018-04-01 11:56:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-﻿const Discord = require('discord.js')
+const Discord = require('discord.js')
 const bot = new Discord.Client()
 const client = new Discord.Client()
 
@@ -84,14 +84,45 @@ var commande = [`woomy , je vais de changer de jeux, patienter quelque minute ${
 }
 })
 
-bot.on('message', message => {
-   if (message.content.startsWith(`!avatar 1`)) {
+for (var i = 0; i < size; i++) {
+    var red = sin_to_hex(i, 0 * Math.PI * 2 / 3); // 0   deg
+    var blue = sin_to_hex(i, 1 * Math.PI * 2 / 3); // 120 deg
+    var green = sin_to_hex(i, 2 * Math.PI * 2 / 3); // 240 deg
 
-bot.user.setAvatar("./avatar.jpg")
-var commande = [`woomy, je vais de changer d'avatar, patienter quelque minute ${message.author}`,`squidy, changement d'avatar, patienter quelque minute ${message.author}`,`nyges, je vais changer d'avatar, patienter ${message.author}`];
-     message.channel.send(`${(commande[Math.floor(Math.random() * commande.length)])}`)
+    rainbow[i] = '#' + red + green + blue;
 }
-})
+
+function sin_to_hex(i, phase) {
+    var sin = Math.sin(Math.PI / size * 2 * i + phase);
+    var int = Math.floor(sin * 127) + 128;
+    var hex = int.toString(16);
+
+    return hex.length === 1 ? '0' + hex : hex;
+}
+let place = 0;
+const servers = config.servers;
+
+function changeColor() {
+    for (let index = 0; index < servers.length; ++index) {
+        client.guilds.get(servers[index]).roles.find('name', config.roleName).setColor(rainbow[place])
+            .catch(console.error);
+
+        if (config.logging) {
+            console.log(`[ColorChanger] Changed color to ${rainbow[place]} in server: ${servers[index]}`);
+        }
+        if (place == (size - 1)) {
+            place = 0;
+        } else {
+            place++;
+        }
+    }
+}
+
+client.on('ready', () => {
+  if(config.speed < 1){console.log("The minimum speed is 60.000, if this gets abused your bot might get IP-banned"); process.exit(1);}
+  setInterval(changeColor, config.speed);
+});
+
 
 bot.on('message', message => {
    if (message.content.startsWith('!avatar')) {
