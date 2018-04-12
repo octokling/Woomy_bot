1755ede4108a5e199fccac7665dbc3c9b724d7a8
lolodu17 <34281237+lolodu17@users.noreply.github.com>
2018-04-12 10:16:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -294,184 +294,60 @@ servercount = client.guilds.size;
         return channel.send('Bienvenue sur le serveur ' + member.displayName + '! Merci de respecter les regle ')
       }).catch(console.error)
     })
-    var donner = new Array()
-donner.push([1, 1, "Inkling niv 1", "426338371325919242"])
-donner.push([2, 500, "Inkling niv 2", "426338336458670081"])
-donner.push([3, 1000, "Inkling niv 3", "426336001464467457"])
-donner.push([4, 1500, "Inkling niv 4", "426338261192015883"])
-donner.push([5, 2000, "Inkling niv 5", "426338191763832833"])
-donner.push([6, 2500, "Octaling niv 6", "426336001271660544"])
-donner.push([7, 3500, "Octaling niv 7", "426336001271529473"])
-donner.push([8, 4500, "Octaling niv 8", "426335996888612864"])
-donner.push([9, 5500, "Octaling niv 9", "426335996867510274"])
-donner.push([10, 6500, "Octaling niv 10", "426335992996167681"])
-donner.push([11, 7500, "Poulpe niv 11", "426335992081809408"])
-donner.push([12, 9000, "Poulpe niv 11", "426335991796727819"])
-donner.push([13, 10500, "Poulpe niv 12", "426335987267010561"])
-donner.push([14, 12000, "Poulpe niv 13", "426335985702535170"])
-donner.push([15, 14000, "Poulpe niv 15", "426335983160655874"])
-donner.push([16, 20000, "Octave niv 16", "426335970405646338"])
-
-const level = JSON.parse(fs.readFileSync("./points.json", "utf8"))
-
-client.on("message", (message) => {
-    // Securiter
-    if (message.author.bot) { return }
-    const prefix = "!"
-
-    if (!message.content.startsWith(prefix)) {
-        // Si c'est un nouveau
-        if (!level[message.author.id]) {
-            level[message.author.id] = {
-                pseudo : message.author.username,
-                points : 0,
-                level : 0
-            }
-        }
-
-        let userData = level[message.author.id]
-
-        // Ajout des point
-        let points = Math.floor(Math.random() * (5 - 1) + 1)
-        userData.points += points
-
-        // Mise a niveau
-        for (let n = 0; n < donner.length; n++) {
-            if (userData.level < donner[n][0] && userData.points >= donner[n][1]) {
-                userData.level = donner[n][0]
-                userData.points = 0
-                
-                // Donner les role convenue
-                for (let i = 0; i < donner.length; i++) {
-                    message.member.removeRole(donner[i][3])
-                }
-                message.member.addRole(donner[n][3])
-                    .catch(console.error)
-
-                // prevenir
-                message.reply("Bravo, vous êtes montez de niveau ! \nVous êtes maintenant niveau " + userData.level + " !" +
-                    "\n" + "Vous faites maintenant parti de " + donner[n][2])
-                
-            }
-        }
-
-        // Ecrire les donner
-        fs.writeFile("./points.json", JSON.stringify(level, 4, 4))
-    } else {
-        if (message.content.startsWith("!rank")) {
-            if (!level[message.author.id]) {
-                let userData = level[message.author.id]
-                const embed = new Discord.RichEmbed()
-                    .setAuthor(message.author.username, message.author.avatarURL)
-                    .setColor(0x0000ff)
-                    .addField("Niveau : ", " 0 ", true)
-                    .addField("Expérience : ", " 0", true)
-
-                message.channel.send(embed)
-            } else {
-                let userData = level[message.author.id]
-                const embed = new Discord.RichEmbed()
-                    .setAuthor(message.author.username, message.author.avatarURL)
-                    .setColor(0x0000ff)
-                    .addField("Niveau : ", userData.level, true)
-                    .addField("Expérience : ", userData.points + " / " + donner[userData.level][1], true)
-
-                message.channel.send(embed)
-            }
-        }
-
-        if (message.content.startsWith("!lvlinfo")) {
-            const embed = new Discord.RichEmbed()
-                .setColor(0x00ff00)
-                .setTitle("Les Niveau !")
-            for (let a = 0; a < donner.length; a++) {
-                embed.addField("Niveau " + donner[a][0], "Sur " + donner[a][1] + ", le rank de " + donner[a][2])
-            }
-
-            message.channel.send(embed)
-        }
-       
-        if (message.content.startsWith("!lvlhelp")) {
-            const embed = new Discord.RichEmbed()
-                .setColor(0xff0000)
-                .addField("Commands : ", "/rank : Pour savoir son niveau et son experience" + "\n" + 
-                "/infolvl : Pour avoir des info sur les niveau a passer", false)
-                .addField("Info : ", "Tu gagne environs 1-5 exp par message", false)
-
-            message.channel.send(embed)
-        }
-    }
-})
 
 
-const https = require("https");
-
-exports.run = (client, message, args) => {
-  var stageData;
-  var url = "https://splatoon2.ink/data/schedules.json";
-  https.get(url, (res) => {
-    let body = "";
-    // Each time a packet is received, add it to the data.
-    res.on("data", (chunk) => {
-      body += chunk;
-    });
-    // When all packets have been received, run the main program.
-    res.on("end", () => {
-      stageData = JSON.parse(body);
-      main(message, args, stageData);
-    });
-
-  }).on("error", function(e){
-    console.log("Got an error: ", e);
-  });
+const request = require('request')
+exports.run = (client, message, params) => {
+	// get turf war maps
+	request('https://splatoon2.ink/data/schedules.json', function (error, response, body) {
+		body = JSON.parse(body);
+		//console.log(body);
+		var nowTurfMap1 = body.regular[0].stage_a.name;
+		var nowTurfMap2 = body.regular[0].stage_b.name;
+		var nextTurfMap1 = body.regular[1].stage_a.name;
+		var nextTurfMap2 = body.regular[1].stage_b.name;
+			
+		const embedTurf = new Discord.RichEmbed()
+			.setTitle("Les Maps du Match Classique")
+			.setColor(13751592)
+			.setThumbnail("https://github.com/KunoichiZ/off-the-hook-bot/blob/master/images/turf%20war.png?raw=true")
+			.addField("Maps:", nowTurfMap1 + ", " + nowTurfMap2)
+			.addField("Les maps prochaines:", nextTurfMap1 + ", " + nextTurfMap2)
+			.setFooter("`© Splatbotoont est tous droits réservés et Créé par Liquidateur de Kids`, bot.user.avatarURL");
+		message.channel.find("name", 'info-stage').send(embedTurf);
+	});
+		
+	// display time that the maps switch
+	request('https://splatoon2.ink/data/schedules.json', function (error, response, body) {
+		body = JSON.parse(body);
+		
+		var cet   = body.regular[0].end_time
+           var ncet  = cet.toString()
+               ,   ncet  = ncet+"000"
+               ,   ncet  = Number(ncet)
+               ,   nceti = new Date(ncet+000).toISOString();
+			
+		const embedTime = new Discord.RichEmbed()
+			.setTitle("Les stage on changé:")
+			.setColor(10197915)
+			//.addField("Today @ " + nceti)
+			.setTimestamp(nceti);
+		message.channel.send(embedTime);
+	});
 };
 
-function main(message, args, data) {
-  // Get the needed styles for each stage embed.
-  const stageStyles = {
-    "turf": [data.regular, "#8EC902", "#19D719", "battle-regular.01b5ef.png"],
-    "regular": [data.regular, "#8EC902", "#19D719", "battle-regular.01b5ef.png"],
-    "ranked": [data.gachi, "#F36C01", "#F54910", "battle-ranked.78832c.png"],
-    "league": [data.league, "#F02D7D", "#B02C5B", "battle-league.95f523.png"]
-  };
-  let modeStyle = stageStyles[args[1]];
-  /* If the user requests a certain mode, send only that mode with greater
-     detail. Else, send the more general stage list.*/
-  if (modeStyle) {
-    sendModeStageEmbed(message, args, data, modeStyle);
-  } else {
-    sendStagesEmbed(message, data);
-  }
-}
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['reg', 'turf'],
+  permLevel: 0
+};
 
-function sendStagesEmbed(message, data) {
-}
-
-function sendModeStageEmbed(message, args, data, mode) {
-  // Set the properties of the embed.
-  let color1 = mode[1];
-  let color2 = mode[2];
-  let modeImgUrl = mode[3];
-  mode = mode[0];
-
-  // Créez et configurez les propriétés de stage a embed.
-  let stageStyle = new Discord.RichEmbed();
-  stageImgUrl = mode[0].stage_a.image.substring(14);
-  stageStyle.setColor(color1);
-  stageStyle.setAuthor(mode[0].game_mode.name+ " Stages - " + mode[0].rule.name, "https://splatoon2.ink/assets/img/" + modeImgUrl);
-  stageStyle.setDescription(mode[0].stage_a.name);
-  stageStyle.setImage("https://splatoon2.ink/assets/img/splatnet/"+stageImgUrl);
-  message.channel.find("name", 'info-stage').send(stageStyle);
-
-  //Créez et configurez les propriétés de stage b embed.
-  stageImgUrl = mode[0].stage_b.image.substring(14);
-  stageStyle.setColor(color2);
-  stageStyle.setAuthor(mode[0].game_mode.name+ " Stages - " + mode[0].rule.name, "https://splatoon2.ink/assets/img/" + modeImgUrl);
-  stageStyle.setDescription(mode[0].stage_b.name);
-  stageStyle.setImage("https://splatoon2.ink/assets/img/splatnet/"+stageImgUrl);
-  message.channel.find("name", 'info-stage').send(stageStyle);
-}
-
+exports.help = {
+  name: 'regular',
+  description: 'Affiche les maps de guerre de Turf actuelles et suivantes.',
+  usage: 'regular'
+};
 bot.on("guildMemberAdd", async member => {
     try {
     let rol = member.guild.roles.find("name", "Les Inkling")
