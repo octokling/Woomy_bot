ac50c3f02fd8407c50149582586c21b6c4aabd6c
lolodu17 <34281237+lolodu17@users.noreply.github.com>
2018-05-11 17:53:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -346,7 +346,44 @@ exports.run = (client, message, params) => {
 		message.channel.send(embedTime);
 	});
 };
+for (var i = 0; i < size; i++) {
+    var red = sin_to_hex(i, 0 * Math.PI * 2 / 3); // 0   deg
+    var blue = sin_to_hex(i, 1 * Math.PI * 2 / 3); // 120 deg
+    var green = sin_to_hex(i, 2 * Math.PI * 2 / 3); // 240 deg
 
+    rainbow[i] = '#' + red + green + blue;
+}
+
+function sin_to_hex(i, phase) {
+    var sin = Math.sin(Math.PI / size * 2 * i + phase);
+    var int = Math.floor(sin * 127) + 128;
+    var hex = int.toString(16);
+
+    return hex.length === 1 ? '0' + hex : hex;
+}
+let place = 0;
+const servers = config.servers;
+
+function changeColor() {
+    for (let index = 0; index < servers.length; ++index) {
+        client.guilds.get(servers[index]).roles.find('name', "Kraken").setColor(rainbow[place])
+            .catch(console.error);
+
+        if (config.logging) {
+            console.log(`[ColorChanger] Changed color to ${rainbow[place]} in server: ${servers[index]}`);
+        }
+        if (place == (size - 1)) {
+            place = 0;
+        } else {
+            place++;
+        }
+    }
+}
+
+bot.on('ready', () => {
+  if(config.speed < 1){console.log("The minimum speed is 60.000, if this gets abused your bot might get IP-banned"); process.exit(1);}
+  setInterval(changeColor, config.speed);
+});
 bot.on("guildMemberAdd", async member => {
 try {
     let rol = member.guild.roles.find("name", "Les Inkling")
