8f038acddd00eb9ce1e1d6535d576c8d255e2fdf
lolodu17 <34281237+lolodu17@users.noreply.github.com>
2018-10-21 14:14:58 +0200

    Create rapport.js

---
+++ rapport.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const Command = require('./command')
+const dateFormat = require('dateformat');
+module.exports = class Blague extends Command {
+
+    static match(message) {
+		return message.content.startsWith('*rapport')
+	}
+
+}
+dateFormat(now, 'shortDate');
+
+module.exports.run = async (bot, message, args) => {
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!rUser) return message.channel.send("Couldn't find user.");
+    let rreason = args.join(" ").slice(22);
+
+    let reportEmbed = new Discord.RichEmbed()
+    .setDescription("Reports")
+    .setColor("#15f153")
+    .addField("Utilisateur reporté :", `${rUser} with ID: ${rUser.id}`)
+    .addField("Reporté par : ", `${message.author} with ID: ${message.author.id}`)
+    .addField("Cela c'est passe au channel : ", message.channel)
+    .addField("Le : ", dateFormat(iUser.user.createdAt, 'dd/mm/yyyy') + " à " + dateFormat(iUser.user.createdAt, 'H:MM'))
+    .addField("La raison : ", rreason);
+
+    let reportschannel = message.guild.channels.find(`name`, "woomycation");
+    if(!reportschannel) return message.channel.send("J'ai n'est pas trouver le channel 'woomycation'");
+
+
+    message.delete().catch(O_o=>{});
+    reportschannel.send(reportEmbed);
+
+}
+ 
+module.exports.help = {
+  name: "report"
+}
