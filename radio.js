31d8d2eb9ead31078e421ef7bfaba3c5a60e45e9
Octokling(1).js <34281237+lolodu17@users.noreply.github.com>
2019-02-02 14:14:13 +0100

    Update radio.js

--- radio.js
+++ radio.js
@@ -6,8 +6,8 @@ var http = require('http');
 bot.on('message', message =>{
 exports.run = (message, args, prefix, bot) => {
 	
-	const command = message.content.split(" ")[0].substring(config.prefix.length);
-  const suffix = message.content.substring(command.length + config.prefix.length + 1);
+	const command = message.content.split(" ")[0].substring(prefix.length);
+  const suffix = message.content.substring(command.length + prefix.length + 1);
       let radio; // Variable vide
 			if (suffix.toLowerCase() == "FunRadio") {
 				radio = "http://streaming.radio.funradio.fr/fun-1-48-192";
