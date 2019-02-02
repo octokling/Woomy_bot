186a3eb5923d184164b45cf96b68539dbf1a80a3
Octokling(1).js <34281237+lolodu17@users.noreply.github.com>
2019-02-02 13:45:13 +0100

    Update radio.js

--- radio.js
+++ radio.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 const bot = new Discord.Client()
 const config = bot.config = require('./config.json');
 var http = require('http');
-exports.run = (message, args, prefix, bot, length) => {
+exports.run = (message, args, prefix, bot) => {
 	bot.on('message', message =>{
 	const command = message.content.split(" ")[0].substring(config.prefix.length);
   const suffix = message.content.substring(command.length + config.prefix.length + 1);
