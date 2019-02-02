a2b72dbaaed6c2f80607124babb6a133fbf0bc1f
Octokling(1).js <34281237+lolodu17@users.noreply.github.com>
2019-02-02 14:31:25 +0100

    Update radio.js

--- radio.js
+++ radio.js
@@ -3,12 +3,11 @@ const Discord = require('discord.js')
 const bot = new Discord.Client()
 const config = bot.config = require('./config.json');
 var http = require('http');
-
-module.exports = class radio extends Command {
-	bot.on('message', message =>{
-	if (message.content.startWith("!radio")
-	const command = message.content.split(" ")[0].substring(prefix.length);
-  const suffix = message.content.substring(command.length + prefix.length + 1);
+bot.on('message', message =>{
+exports.run = (message, args, prefix, bot) => {
+	
+	const command = message.content.split(" ")[0].substring(config.prefix.length);
+  const suffix = message.content.substring(command.length + config.prefix.length + 1);
       let radio; // Variable vide
 			if (suffix.toLowerCase() == "FunRadio") {
 				radio = "http://streaming.radio.funradio.fr/fun-1-48-192";
