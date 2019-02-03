83ef3ddca3a75d0b4d3c8850faf25c8efbcec1d6
Octokling(1).js <34281237+lolodu17@users.noreply.github.com>
2019-02-03 09:24:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -331,30 +331,7 @@ let embed = new Discord.RichEmbed()
       let radio; // Variable vide
 			if (music == "FunRadio") {
 				radio = "http://streaming.radio.funradio.fr/fun-1-48-192";
-        
-			} else if (music == "FranceBleu") {
-				radio = "http://chai5she.cdn.dvmr.fr/fblarochelle-midfi.mp3";
-        
-			} else if (music == "Mixx") {
-				radio = "http://broadcast.infomaniak.net/mixxfmfr-192.mp3";
-        
-      }else if (music == "NRJ") {
-        radio = "http://streaming.radio.funradio.fr/fun-1-48-192";
-        
-          } else if (music == "VirginRadio") {
-				radio = "http://vr-live-mp3-128.scdn.arkena.com/virginradio.mp3";
-		  
-		  } else if (music == "VinciAutoroute") {
-				radio = "http://str0.creacast.com/radio_vinci_autoroutes_8";
-            
-			} else {
-        let embed = new Discord.RichEmbed()
-				.setDescription("Liste des Radio :")
-				.setColor("#b92727")
-        .addField("Vous devez choisir entre ces radio :", "FunRadio ; FranceBleu ; Mixx ; NRJ ; VirginRadio .")
-				return message.channel.send({ embed });
-			}
-       const voiceChannel = message.member.voiceChannel;
+         const voiceChannel = message.member.voiceChannel;
     if (!voiceChannel) {
       return message.reply('Soyez dans un channel je vous rejoint');
       message.member.voiceChannel.join().then(connection => {
@@ -365,6 +342,79 @@ let embed = new Discord.RichEmbed()
 					.setDescription("Radio activé")
 					message.channel.send({ embed });
 				});
+			} else if (music == "FranceBleu") {
+				radio = "http://chai5she.cdn.dvmr.fr/fblarochelle-midfi.mp3";
+         const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel) {
+      return message.reply('Soyez dans un channel je vous rejoint');
+      message.member.voiceChannel.join().then(connection => {
+				require('http').get(radio, (res) => {
+					connection.playStream(res);
+           let embed = new Discord.RichEmbed()
+					.setColor("#b92727")
+					.setDescription("Radio activé")
+					message.channel.send({ embed });
+				});
+			} else if (music == "Mixx") {
+				radio = "http://broadcast.infomaniak.net/mixxfmfr-192.mp3";
+         const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel) {
+      return message.reply('Soyez dans un channel je vous rejoint');
+      message.member.voiceChannel.join().then(connection => {
+				require('http').get(radio, (res) => {
+					connection.playStream(res);
+           let embed = new Discord.RichEmbed()
+					.setColor("#b92727")
+					.setDescription("Radio activé")
+					message.channel.send({ embed });
+				});
+      }else if (music == "NRJ") {
+        radio = "http://streaming.radio.funradio.fr/fun-1-48-192";
+         const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel) {
+      return message.reply('Soyez dans un channel je vous rejoint');
+      message.member.voiceChannel.join().then(connection => {
+				require('http').get(radio, (res) => {
+					connection.playStream(res);
+           let embed = new Discord.RichEmbed()
+					.setColor("#b92727")
+					.setDescription("Radio activé")
+					message.channel.send({ embed });
+				});
+          } else if (music == "VirginRadio") {
+				radio = "http://vr-live-mp3-128.scdn.arkena.com/virginradio.mp3";
+		   const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel) {
+      return message.reply('Soyez dans un channel je vous rejoint');
+      message.member.voiceChannel.join().then(connection => {
+				require('http').get(radio, (res) => {
+					connection.playStream(res);
+           let embed = new Discord.RichEmbed()
+					.setColor("#b92727")
+					.setDescription("Radio activé")
+					message.channel.send({ embed });
+				});
+		  } else if (music == "VinciAutoroute") {
+				radio = "http://str0.creacast.com/radio_vinci_autoroutes_8";
+             const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel) {
+      return message.reply('Soyez dans un channel je vous rejoint');
+      message.member.voiceChannel.join().then(connection => {
+				require('http').get(radio, (res) => {
+					connection.playStream(res);
+           let embed = new Discord.RichEmbed()
+					.setColor("#b92727")
+					.setDescription("Radio activé")
+					message.channel.send({ embed });
+				});
+			} else {
+        let embed = new Discord.RichEmbed()
+				.setDescription("Liste des Radio :")
+				.setColor("#b92727")
+        .addField("Vous devez choisir entre ces radio :", "FunRadio ; FranceBleu ; Mixx ; NRJ ; VirginRadio .")
+				return message.channel.send({ embed });
+			}
+      
 			}).catch(err => "**Erreur:** ```\n" + err + "```");
 			}
 	}})
